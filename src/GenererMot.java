0538bb6b7de7d2c1b4fccc423bf50cbd170cd997
NaelGuenfoudi <guenfmen@gmail.com>
2022-02-09 22:08:48 +0100

    projet Motus

---
+++ src/GenererMot.java
@@ -0,0 +1,91 @@
+import java.util.Objects;
+import java.util.Random;
+
+/**
+ * Classe qui choisit au hasard un mots d'après un fichier
+ */
+public class GenererMot {
+
+
+    static LectureFichier fich = new LectureFichier("dico/ListeMotFacile.txt");
+    /**
+     * liste des mots d'un fichier Dico
+     */
+    public static String[] listeMots = fich.lireFichier();
+    /**
+     * mot généré au hasard
+     */
+    private String motG;
+
+    public static void main(String[] args) {
+        GenererMot gM = new GenererMot();
+        String[] tab = gM.choisirMots(5);
+        gM.choisirMot(tab);
+        System.out.println(gM.getMotG());
+    }
+
+    /**
+     * cree un tableau de mots de longueur choisi
+     *
+     * @param niv niveau du mots(longueur)
+     * @return tableau de mots
+     */
+    private String[] choisirMots(int niv) {
+        String[] tabNivMot = new String[0];
+        for (String mot :
+                listeMots) {
+            if (mot.length() == niv) {
+                String[] tabNouv2 = new String[tabNivMot.length + 1];//1
+
+                for (int i = 0; i < tabNivMot.length; i++) {
+                    tabNouv2[i] = tabNivMot[i];
+                }
+                tabNouv2[tabNivMot.length] = mot;
+                tabNivMot = tabNouv2;
+            }
+
+        }
+        return tabNivMot;
+    }
+
+    /**
+     * definit motG par un mot au hasard d'un tableau de mots et enleve ce même mots de la liste des mots du dico
+     *
+     * @param tabMot
+     */
+    public void choisirMot(String[] tabMot) {
+        Random r = new Random();
+        int rand = r.nextInt(tabMot.length);
+        motG = tabMot[rand];
+        enleverMot();
+    }
+
+    /**
+     * enleve un mot d'un tableau
+     */
+    private void enleverMot() {
+        String[] listeMot2 = new String[listeMots.length - 1];
+        boolean trouve = false;
+        for (int i = 0; i < listeMot2.length; i++) {
+            if (Objects.equals(listeMots[i], motG)) {
+                i++;
+                trouve = true;
+            }
+            if (!trouve) {
+                listeMot2[i] = listeMots[i];
+            } else
+                listeMot2[i - 1] = listeMots[i];
+        }
+        listeMots = listeMot2;
+    }
+
+    public String getMotG() {
+        return motG;
+    }
+
+    public void setMotG(String motG) {
+        this.motG = motG;
+    }
+
+}
+
